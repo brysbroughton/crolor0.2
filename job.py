41f9a507eb6826d5f6d19ce47c741a313f1208fd
unknown <wrighta@DT027929.otc.edu>
2015-04-21 13:58:21 -0500

    Added Log class to crol.py

---
+++ job.py
@@ -0,0 +1,13 @@
+from crol import *
+from otc_department_registry import registry_data as rd
+rg = Registry(rd)
+
+class Job:
+
+    visited_urls = set([])
+    
+
+    def __init__(self):
+        pass
+        
+    
