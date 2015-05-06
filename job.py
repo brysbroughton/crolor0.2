0b312c4ac1ec4c02fe3590a6be2c891d8320fdab
Brys Broughton <broughtb@otc.edu>
2015-05-06 09:52:30 -0500

    Removed redundant email code from CrawlJob.go()

--- job.py
+++ job.py
@@ -51,14 +51,6 @@ class CrawlJob(crol.GenericType):
         self.log.closefile()
         if self.has_broken_links:
             self.applyactions()
-            report_location = self.log.path+self.log.filename+self.log.endfilename
-            msg = '<h1>Link Report</h1><p>You can review the report at: <a href="' + report_location + '">this link</a></p>'
-            subject = 'Crawl Completed'
-            to_address = self.registration.department.main_email
-            from_address = 'web@otc.edu'
-            email_props = {'to_address':to_address, 'from_address':from_address, 'subject':subject, 'msg_body':msg}
-            e = crol.Email(email_props)
-            e.send()
     
     def lognode(self, node):
         if str(node.status) == '404':
