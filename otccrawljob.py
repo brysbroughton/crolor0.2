4c4a564a0efb909de51a7078d6cfe7ca531792e2
Vulpenox <albeofox@gmail.com>
2015-05-08 09:58:27 -0500

    crawljob duration timer test in otccrawljob.py

--- otccrawljob.py
+++ otccrawljob.py
@@ -1,8 +1,12 @@
 import job, crol, otcregistry
+import time
 
 def start():
     for r in otcregistry.registrations:
         cj = job.CrawlJob({'registration':r})
         cj.go()
 
+time_start = time.clock()
 start()
+time_stop = time.clock()
+print 'This crawljob took approximately ' + str(round(time_stop - time_start, 2)) + ' seconds to complete.'
